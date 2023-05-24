d7aef6da10afa6394286c4a7901ad1d4cfc57551
Kevin Stark <kstark734@gmail.com>
2023-05-24 21:11:11 +0100

    create OurClientsMarketing component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import MarketingLandingHero from "@/components/MarketingLandingHero/MarketingLandingHero";
+import OurClientsMarketing from "@/components/OurClients/OurClientsMarketing";
+import {_brands} from '@/_mock'
 
 export default function Home() {
   return (
     <>
       <MarketingLandingHero/>
+        <OurClientsMarketing brands={_brands}/>
 
     </>
   )
