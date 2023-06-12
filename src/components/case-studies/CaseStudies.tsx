db7507751a11bc48d59435727874f3510063b9d2
Kevin Stark <kstark734@gmail.com>
2023-06-12 12:39:55 +0100

    Create case study component view

---
+++ src/components/case-studies/CaseStudies.tsx
@@ -0,0 +1,40 @@
+// @mui
+import { Container, Typography, Stack } from '@mui/material';
+// _mock
+import { _caseStudies, _blogMarketingPosts, _testimonials } from '@/_mock';
+//
+import LatestPosts from "@/components/blog/latest-posts/LatestPosts";
+import HomeTestimonial from "@/components/homepage-components/HomeTestimonial/HomeTestimonial";
+import { MarketingCaseStudyList } from './list';
+
+// ----------------------------------------------------------------------
+
+export default function MarketingCaseStudiesView() {
+    return (
+        <>
+            <Container>
+                <Stack
+                    spacing={3}
+                    sx={{
+                        py: 5,
+                        textAlign: { xs: 'center', md: 'left' },
+                    }}
+                >
+                    <Typography variant="h2">Our Case Studies</Typography>
+
+                    <Typography sx={{ color: 'text.secondary' }}>
+                        Nullam tincidunt adipiscing enim.
+                        <br /> Mauris sollicitudin fermentum libero.
+                    </Typography>
+                </Stack>
+
+                <MarketingCaseStudyList caseStudies={_caseStudies} />
+            </Container>
+
+            <HomeTestimonial testimonials={_testimonials} />
+
+            <LatestPosts posts={_blogMarketingPosts.slice(0, 4)} />
+
+        </>
+    );
+}
---
+++ src/pages/case-studies/index.tsx
@@ -0,0 +1,5 @@
+export default function CaseStudies() {
+    return (
+        <div><h2>Case Studies Page</h2></div>
+    )
+}
