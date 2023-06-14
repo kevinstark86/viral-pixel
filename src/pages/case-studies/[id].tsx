d73901f44a7de2f8dded838c414d24474527d4b9
Kevin Stark <kstark734@gmail.com>
2023-06-14 12:40:24 +0100

    Create blog posts view

--- src/pages/case-studies/[id].tsx
+++ src/pages/case-studies/[id].tsx
@@ -1,4 +1,7 @@
 import CaseStudy from "@/components/case-study/CaseStudy";
+import MainLayout from "@/layouts/main";
+
+SingleCaseStudy.getLayout = (page: React.ReactElement) => <MainLayout>{page}</MainLayout>
 
 export default function SingleCaseStudy() {
     return (
