22cde8fbe3c6723b76a6d2ce78b149b8f70ce575
Kevin Stark <kstark734@gmail.com>
2023-06-14 13:31:01 +0100

    Create dynamic blog page view

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,5 +1,143 @@
-export default function Post() {
+import { useState } from 'react';
+// @mui
+import {
+    Stack,
+    Avatar,
+    Divider,
+    Popover,
+    Checkbox,
+    MenuItem,
+    Container,
+    Typography,
+    IconButton,
+    Unstable_Grid2 as Grid,
+} from '@mui/material';
+// layout
+import MainLayout from "@/layouts/main";
+// routes
+import { paths } from '@/routes/paths';
+// utils
+import { fDate } from '@/utils/formatTime';
+// _mock
+import { _blogCareerPosts, _socials } from '@/_mock';
+// components
+import Iconify from '@/components/common/Iconify';
+import Markdown from '@/components/common/markdown';
+import CustomBreadcrumbs from '@/components/common/custom-breadcrumbs';
+//
+import NewsletterCareer from '@/components/newsletter/career';
+import { BlogCareerLatestPosts } from '@/components/blog/career';
+import { PostTags, PostAuthor, PostTimeBlock, PostSocialsShare } from '@/components/blog/components';
+
+// ----------------------------------------------------------------------
+
+
+CareerPostView.getLayout = (page: React.ReactElement) => <MainLayout>{page}</MainLayout>
+
+export default function CareerPostView() {
+    const { title, description, duration, createdAt, favorited, author, tags, content } =
+        _blogCareerPosts[0];
+
+    const [favorite, setFavorite] = useState(favorited);
+
+    const [open, setOpen] = useState<HTMLElement | null>(null);
+
+    const handleOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setOpen(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setOpen(null);
+    };
+
+    const handleChangeFavorite = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFavorite(event.target.checked);
+    };
+
     return (
-        <div><h1>Blog Post Page</h1></div>
-    )
+        <>
+            <Divider />
+
+            <Container sx={{ overflow: 'hidden' }}>
+                <Grid container spacing={3} justifyContent={{ md: 'center' }}>
+                    <Grid xs={12} md={8}>
+                        <CustomBreadcrumbs
+                            links={[
+                                { name: 'Home', href: '/' },
+                                { name: 'Blog', href: paths.career.posts },
+                                { name: title },
+                            ]}
+                            sx={{ my: 5 }}
+                        />
+
+                        <Typography variant="h2" component="h1">
+                            {title}
+                        </Typography>
+
+                        <Stack direction="row" justifyContent="space-between" spacing={1.5} sx={{ my: 5 }}>
+                            <Avatar src={author.picture} sx={{ width: 48, height: 48 }} />
+
+                            <Stack spacing={0.5} flexGrow={1}>
+                                <Typography variant="subtitle2">{author.name}</Typography>
+
+                                <PostTimeBlock createdAt={fDate(createdAt)} duration={duration} />
+                            </Stack>
+
+                            <Stack direction="row" alignItems="center">
+                                <IconButton onClick={handleOpen} color={open ? 'primary' : 'default'}>
+                                    <Iconify icon="carbon:share" />
+                                </IconButton>
+
+                                <Checkbox
+                                    color="error"
+                                    checked={favorite}
+                                    onChange={handleChangeFavorite}
+                                    icon={<Iconify icon="carbon:favorite" />}
+                                    checkedIcon={<Iconify icon="carbon:favorite-filled" />}
+                                />
+                            </Stack>
+                        </Stack>
+
+                        <Typography variant="h5" sx={{ mb: 5 }}>
+                            {description}
+                        </Typography>
+
+                        <Markdown content={content} firstLetter />
+
+                        <PostTags tags={tags} />
+
+                        <PostSocialsShare />
+
+                        <Divider sx={{ mt: 8 }} />
+
+                        <PostAuthor author={author} />
+                    </Grid>
+                </Grid>
+            </Container>
+
+            <Divider />
+
+            <BlogCareerLatestPosts posts={_blogCareerPosts.slice(0, 5)} />
+
+            <NewsletterCareer />
+
+            <Popover
+                open={!!open}
+                onClose={handleClose}
+                anchorEl={open}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+                PaperProps={{
+                    sx: { width: 220, p: 1 },
+                }}
+            >
+                {_socials.map((social) => (
+                    <MenuItem key={social.value} onClick={handleClose} sx={{ typography: 'body2' }}>
+                        <Iconify icon={social.icon} width={24} sx={{ mr: 1, color: social.color }} />
+                        Share via {social.label}
+                    </MenuItem>
+                ))}
+            </Popover>
+        </>
+    );
 }
